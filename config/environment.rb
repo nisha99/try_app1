b9b57b62f94b131bcb5155cbad05b926d95faacf
Nisha Thapa <nisha.cthapa@gmail.com>
2014-07-15 10:40:08 -0500

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+TryApp1::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TryApp1::Application.config.secret_key_base = '8861a823d70d7b976a47444ad1c5919958e8f5c3204b32e8dd402f3692fe310b2b655025dd100aa33312b904c9b89b82a0dcfe536b3378b1572c9e93983c61c5'
